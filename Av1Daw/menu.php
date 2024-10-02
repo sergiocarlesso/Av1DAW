b4674f2487ded1bec4a49e60fa28a452a0f20a92
sergiocarlesso <sergiocarlessof@hotmail.com>
2024-10-02 16:59:39 -0300

    Update menu.php

--- Av1Daw/menu.php
+++ Av1Daw/menu.php
@@ -7,9 +7,12 @@
 <body>
 	<h1>Perguntas e respostas</h1>
 	<ul>
-    <li><a href="adicionarusuario.php">Adicionar novo usuario</a></li>
-    <li><a href="adicionarPeR.php">Adicionar uma nova Pergunta e Respostas</a></li>
-    <li><a href="listarperguntas.php">Listas todas as perguntas</a></li>
+    		<li><a href="adicionarusuario.php">Adicionar novo usuario</a></li>
+   		<li><a href="adicionarPeR.php">Adicionar uma nova Pergunta e Respostas</a></li>
+    		<li><a href="listarperguntas.php">Listas todas as perguntas</a></li>
+		<li><a href="listaruma.php">Listar uma pergunta</a></li>
+		<li><a href="alterarpergunta.php">Alterar uma pergunta</a></li>
+	 	<li><a href="excluirpergunta.php">Excluir uma pergunta</a></li> 
 	</ul>
 </body>
 </html>
